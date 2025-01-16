afbb2ac4b01679123d9873e4a2cebc3a1e3a4ae5
Gustavo Reis <gustavoazreis@gmail.com>
2025-01-16 15:29:39 -0300

    desafio aula 03

---
+++ aula03/desafio.py
@@ -0,0 +1,48 @@
+print("-="*10,"CALCULADORA DE BÔNUS SALARIAL","-="*10)
+
+# Solicitação ao usuário para input do nome
+while not nome_valido:
+    try:
+        nome = str(input("Digite seu nome: "))
+        # Check se o nome está vazio
+        if len(nome) == 0:
+            raise ValueError("O nome não pode estar vazio.")
+        # Check se tem números no nome
+        elif any(char.isdigit() for char in nome):
+            raise ValueError("O nome não deve conter números.")
+        else:
+            print(f"Nome válido: {nome}")
+            nome_valido = True
+    except ValueError as e:
+        print(e)
+
+# Solicitação ao usuário para input do salário
+while not salario_valido:
+    try:
+        salario = float(input("Digite o valor do seu salário bruto: "))
+        if salario < 0:
+            print("Por favor, digite um valor positivo para o salário.")
+        else:
+            salario_valido = True
+    except ValueError:
+        print("Entrada inválida para o salário. Por favor, digite um número.")
+
+# Solicita ao usuário que digite o valor do bônus recebido
+while not bonus_valido:
+    try:
+        bonus_recebido = float(input("Digite o valor do bônus recebido: "))
+        if bonus_recebido < 0:
+            print("Por favor, digite um valor positivo para o bônus.")
+        else:
+            salario_valido = True
+    except ValueError:
+        print("Entrada inválida para o bônus. Por favor, digite um número")
+
+atingimento = float(input("Digite o percentual de atingimento do bônus:"))
+
+#lógica de cálculo
+bonus_final = bonus_recebido * 1.2
+KPI = (salario + bonus_final)/1000
+
+print(f"Olá {nome}, o valor do seu bônus este ano foi de R${KPI:.2f}.")
+print("-="*10,"ENCERRANDO PROGRAMA","-="*10)    
